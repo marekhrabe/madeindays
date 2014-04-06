0c0581ffdcc47ce01e2b15acf50cb709325ac0a4
marekhrabe <marekhrabe@abdoc.net>
2014-04-06 11:51:40 +0200

    updates

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,20 @@
 var isMobile = !!navigator.userAgent.match(/mobile/i);
 
+// loading
+
+window.addEventListener('load', function () {
+    document.body.classList.add('loaded');
+});
+
+// skrollr responsive logic
+
 var scrolling = null;
 var checkSize = function () {
     if (scrolling && window.innerHeight < 700) {
-        document.body.classList.remove('js');
         scrolling.destroy();
         scrolling = null;
     }
     if (!scrolling && window.innerHeight >= 700) {
-        document.body.classList.add('js');
         scrolling = skrollr.init({
             forceHeight: false,
             edgeStrategy: 'set',
@@ -16,15 +22,28 @@ var checkSize = function () {
     }
 };
 
+// activate skrollr only for non-mobile devices
+
 if (!isMobile) {
     window.addEventListener('resize', checkSize);
     checkSize();
 }
 
-window.addEventListener('load', function () {
-    document.body.classList.add('loaded');
-});
+// buy button
 
 document.getElementById('buy').addEventListener('click', function () {
     document.getElementById('order').classList.add('active');
+});
+
+// measures and geolocation
+
+var imperial = ['US', 'UK', 'CA'];
+var measures = document.getElementById('measures');
+window.geolocated = function (data) {
+    if (data && data.countryCode && imperial.indexOf(data.countryCode) > -1) {
+        measures.classList.add('imperial');
+    }
+};
+measures.addEventListener('click', function () {
+    measures.classList.toggle('imperial');
 });
