a08028b2ea25f8c4b75e9db4b843bb00b27feef2
marekhrabe <marekhrabe@abdoc.net>
2014-04-07 16:36:38 +0200

    before launch edits

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,13 @@
 (function () {
     var isMobile = !!navigator.userAgent.match(/(iphone|ipod|android)/i);
 
+    // share data
+
+    var url = encodeURIComponent('http://' + window.location.host + '/');
+    var messages = {
+        twitter: 'Handcrafted leather goods with personality. Which accessory was made for you? ',
+    };
+
     // loading
 
     window.addEventListener('load', function () {
@@ -49,21 +56,44 @@
 
     // buy button
 
+    var order = document.getElementById('order');
     document.getElementById('buy').addEventListener('click', function () {
-        document.getElementById('order').classList.add('active');
+        order.classList.add('active');
     });
 
     // close overlay
 
-    var closeOverlay = function () {
-        document.getElementById('order').classList.remove('active');
+    var success = document.getElementById('success');
+    var closeOverlays = function () {
+        order.classList.remove('active');
+        success.classList.remove('active');
     };
-    document.getElementById('close').addEventListener('click', closeOverlay);
     window.addEventListener('keydown', function (e) {
         if (e.keyCode === 27) {
-            closeOverlay();
+            closeOverlays();
         }
-    })
+    });
+
+    // global click handlers
+
+    window.addEventListener('click', function (e) {
+        var target = e.target;
+        if (target.tagName === 'A') {
+            if (target.classList.contains('close')) {
+                closeOverlays();
+                e.preventDefault();
+            } else if (target.classList.contains('share')) {
+                var popup = 'height=320, width=640, toolbar=no, menubar=no, scrollbars=no, resizable=no, location=no, directories=no, status=no';
+                if (target.classList.contains('facebook')) {
+                    window.open('https://www.facebook.com/sharer/sharer.php?u=' + url, 'facebook', popup);
+                    e.preventDefault();
+                } else if (target.classList.contains('twitter')) {
+                    window.open('https://twitter.com/home?status=' + encodeURIComponent(messages.twitter) + ' ' + url, 'twitter', popup);
+                    e.preventDefault();
+                }
+            }
+        }
+    });
 
     // measures and geolocation
 
@@ -90,4 +120,49 @@
         }
         animatedScrollTo(document.body, Math.round(nextScreen * window.innerHeight), 1500);
     });
+
+    // order form
+
+    var formError = function () {
+        formcontainer.classList.add('error');
+        setTimeout(function () {
+            formcontainer.classList.remove('error');
+        }, 300);
+    };
+
+    var orderform = document.getElementById('orderform');
+    var formcontainer = document.getElementById('formcontainer');
+    orderform.addEventListener('submit', function (e) {
+        e.preventDefault();
+
+        var name = document.getElementById('name').value;
+        var email = document.getElementById('email').value;
+        var content = document.getElementById('content').value;
+
+        if (name && email && email.indexOf('@') > -1) {
+            document.getElementById('buybutton').innerHTML = 'Sending';
+
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', 'http://madeindays.herokuapp.com/email?email=' + encodeURIComponent(email) + '&name=' + encodeURIComponent(name) + '&content=' + encodeURIComponent(content));
+            xhr.onload = function () {
+                var data = JSON.parse(this.response);
+                if (data.status && data.status === 'ok') {
+                    closeOverlays();
+                    setTimeout(function () {
+                        success.classList.add('active');
+                    });
+                } else {
+                    formError();
+                }
+            };
+            xhr.send();
+        } else {
+            formError();
+        }
+    });
+
+    window.priceData = function (data) {
+        document.getElementById('priceTag').innerHTML = data.price;
+        document.getElementById('piecesLeft').innerHTML = data.piecesLeft;
+    };
 })();
